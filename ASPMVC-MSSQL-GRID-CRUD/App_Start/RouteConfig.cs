cdf11c2b1762751ed92c94beec6c08c86ff0a7bc
amritanshu.verma <amritanshu.verma@outlook.com>
2018-06-22 13:49:36 +0530

    Working Grid Edit not Set

--- ASPMVC-MSSQL-GRID-CRUD/App_Start/RouteConfig.cs
+++ ASPMVC-MSSQL-GRID-CRUD/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace ASPMVC_MSSQL_GRID_CRUD
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "RenderGrid", id = UrlParameter.Optional }
             );
         }
     }
--- ASPMVC-MSSQL-GRID-CRUD/Controllers/HomeController.cs
+++ ASPMVC-MSSQL-GRID-CRUD/Controllers/HomeController.cs
@@ -3,17 +3,53 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using System.Data.Entity;
+using ASPMVC_MSSQL_GRID_CRUD.Models;
+using System.Net;
 
 namespace ASPMVC_MSSQL_GRID_CRUD.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        private GridModel db = new GridModel();
+        public ActionResult RenderGrid()
         {
-            return View();
+            return View(db.Customers.ToList());
+        }
+
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var customer = db.Customers.Find(id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customer);
         }
 
 
+        //public ActionResult Edit([Bind(Include = "CustomerID,CompanyName")] Customer customer)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        db.Entry(customer).State = EntityState.Modified;
+        //        db.SaveChanges();
+        //        return RedirectToAction("RenderGrid");
+        //    }
+        //    return View(customer);
+        //}
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+         
+        }
     }
 }
